358d93de470a6ce56892987c15909cabb706ac0d
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-04 22:13:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,55 @@ class Song
     self.class.all << self
   end
 
+  # class constructors
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{ |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by { |alphabet| alphabet.name }
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    song.name = (filename.split(" - ") [1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ") [0])
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new
+    song.name = (filename.split(" - ") [1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ") [0])
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
